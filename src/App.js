41e7893d238ee7b56ba916c2c1cab41575f35555
najmusskb <najmussakib173@gmail.com>
2022-09-25 14:00:31 +0600

    api data load

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 function App() {
   return (
     <div className="App">
 
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExterUsers></ExterUsers>
+     
      
     </div>
   );
+}
+
+
+
+function ExterUsers(){
+
+
+  // api theke data load korte 5 ta step folllow korte hobe 
+  // step-1
+  const [users,setUsers]= useState([]);
+// step-02
+useEffect(()=>{
+
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res=>res.json())
+  .then(data=>console.log(data))
+},[])
+
+  return(
+
+    <div>
+      <h2>External Users</h2>
+    </div>
+  )
 }
 
 function Counter(){
